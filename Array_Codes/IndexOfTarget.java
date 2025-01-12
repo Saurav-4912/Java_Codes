3332b900657162efad0f541670458dc1d6e7a2b4
Saurav Pawar <sauravpawar46@gmail.com>
2025-01-12 12:10:32 +0530

    Array Code - Find index of target

---
+++ Array_Codes/IndexOfTarget.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class IndexOfTarget {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number : ");
+    int number = sc.nextInt();
+
+    int[] numbers = {14,25,12,53,2,102,72,97};
+    int result = IndexOfTarget.targetElement(number, numbers);
+    if(result == 1){
+      System.out.println("Given number is present in array");
+    }else{
+      System.out.println("Number not found");
+    }
+    sc.close();
+  }
+
+  public static int targetElement(int number , int[] numbers){
+
+    for(int i = 0 ; i < numbers.length ; i++){
+      if(number == numbers[i]){
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+}
