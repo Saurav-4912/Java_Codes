f7a51386f761054138d5ce40713175b50883f66c
Saurav Pawar <sauravpawar46@gmail.com>
2025-01-21 19:38:31 +0530

    Added Code Advance pattern 3

--- Patterns/AdvancePattern2.java
+++ Patterns/AdvancePattern2.java
@@ -1,17 +1,12 @@
-public class AdvancePattren3 {
+public class AdvancePattern2 {
   public static void main(String[] args) {
-    for (int i = 1; i < 5; i++)
-     {
-      for (int j = 4; j >= i; j--) {
+    for(int i = 1 ; i<5 ; i ++){
+      for(int j = 1; j <= i ; j++){
         System.out.print(" ");
       }
-
-      for (int k = 9; k<i*2; k++) {
+      for(int k = 9 ; k > i*2 ; k--){
         System.out.print("*");
       }
-
-      
-
       System.out.println();
     }
   }
