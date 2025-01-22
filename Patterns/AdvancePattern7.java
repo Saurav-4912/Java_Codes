1a0745ddc406098518615b900f7f81927bee439d
Saurav Pawar <sauravpawar46@gmail.com>
2025-01-22 17:12:32 +0530

    Added Code - Advance Pattern 7

---
+++ Patterns/AdvancePattern7.java
@@ -0,0 +1,19 @@
+public class AdvancePattern7 {
+  public static void main(String[] args) {
+     for(int i = 0 ; i<4 ; i++){
+      for(int j = 4 ; j>=i; j--){
+        System.out.print(" ");
+      }
+      System.out.print("*");
+    
+        System.out.println();
+      }
+        for(int i=0; i<4; i++){
+      for(int l=0; l<=i; l++){
+        System.out.print(" ");
+      }
+      System.out.print("*");
+      System.out.println();
+     }}
+  }
+
