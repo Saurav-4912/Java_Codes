a9b33e340c4871ba31f9254c61f2bc929b9253cd
Saurav Pawar <sauravpawar46@gmail.com>
2025-02-18 22:44:29 +0530

    Modified BubbleSort

--- Sorting_Algorithm/BubbleSort.java
+++ Sorting_Algorithm/BubbleSort.java
@@ -1,5 +1,3 @@
-package Sorting_Algorithm;
-
 
 import java.util.Arrays;
 
@@ -7,12 +5,9 @@ public class BubbleSort {
   public static void bubbleSort(int[] arr) {
     int n = arr.length;
 
-    for (int i = n - 1; i > 0; i--) 
-    {
-      for (int j = 0; j < i; j++)
-       {
-        if (arr[j] > arr[j + 1])
-         {
+    for (int i = n - 1; i > 0; i--) {
+      for (int j = 0; j < i; j++) {
+        if (arr[j] > arr[j + 1]) {
           int temp = arr[j];
           arr[j] = arr[j + 1];
           arr[j + 1] = temp;
@@ -23,9 +18,9 @@ public class BubbleSort {
   }
 
   public static void main(String[] args) {
-     int[] arr = { 94, 125, 6, 39, 10 };
+    int[] arr = { 94, 125, 6, 39, 10 };
     // int[] arr = {55,44,33,22,11};
-    //int[] arr = { 11, 22, 33, 44, 55 };
+    // int[] arr = { 11, 22, 33, 44, 55 };
     System.out.println("Before Sort : " + Arrays.toString(arr));
     BubbleSort.bubbleSort(arr);
     System.out.println("After Sort : " + Arrays.toString(arr));
