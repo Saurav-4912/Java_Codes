d39a16cb3b014a811155539b39d969fc92d975bc
Saurav Pawar <sauravpawar46@gmail.com>
2025-03-08 17:05:23 +0530

    Added Comment in Demo.java file

--- LinkedList/Demo.java
+++ LinkedList/Demo.java
@@ -15,14 +15,17 @@ class Node {
 public class Demo {
   public static void main(String[] args) {
 
+    // Creating Object and set Data
     Node f = new Node(11);
     Node s = new Node(22);
     Node l = new Node(33);
 
+    // Logic Of Link Formation
     f.next = s;
     s.next = l;
     l.next = null;
 
+    // Printing logic of data of each node
     System.out.println(f.data);
     System.out.println(f.next.data);
     System.out.println(f.next.next.data);
